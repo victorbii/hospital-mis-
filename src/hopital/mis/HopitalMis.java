72940a75faca650e86b4a78a680edb3149e6fe1d
victorbii <victorbii@users.noreply.github.com>
2019-02-28 02:43:42 +0300

    management information

---
+++ src/hopital/mis/HopitalMis.java
@@ -0,0 +1,14 @@
+/*To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Template
+and open the template in the editor.
+package hopital.mis;/*@author Victor Bii*/
+public class HopitalMis {
+/** @param args the command line arguments */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+    
+}
